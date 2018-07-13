fa0b046217cd78fb15e72cae07daf18927ad11b6
eastrd <yjia285@student.monash.edu>
2018-07-14 00:00:16 +1000

    Add module for checking & saving proxy into MongoDB

---
+++ scrapers/db.py
@@ -0,0 +1,24 @@
+from pymongo import MongoClient
+
+
+def save_new_proxy_record(one_proxy_dict_data):
+    '''Examine the given single proxy dict object and store into "new" collection
+    '''
+    print("Connect to MongoDB...")
+    client = MongoClient("149.28.195.244", 27017, authSource='admin',
+                         username="xiaoyu", password="Jianyuhao123$")
+
+    db = client["proxypool"]
+
+    new_storage = db["new"]
+    all_storage = db["all"]
+
+    # If historical collection contains the proxy information (Key: ip), then ignore
+    if len([i for i in all_storage.find({"ip": one_proxy_dict_data["ip"]})]) > 0:
+        print("Found duplicate proxy for %s, ignore..." % one_proxy_dict_data["ip"])
+        return
+
+    # Since this ip is new, then store this proxy data in "new" collection, and store only its ip in all storage
+    print("Store %s" % one_proxy_dict_data["ip"])
+    new_storage.insert(one_proxy_dict_data)
+    all_storage.insert({"ip" : one_proxy_dict_data["ip"]})
