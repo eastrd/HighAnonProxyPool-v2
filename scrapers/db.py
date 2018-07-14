28cf955e89db73571e90d0202a4fb5f862cc43ec
eastrd <yjia285@student.monash.edu>
2018-07-14 10:18:41 +1000

    Optimize proxy storage logic

--- scrapers/db.py
+++ scrapers/db.py
@@ -17,10 +17,9 @@ def save_new_proxy_record(one_proxy_dict_data):
 
     # If historical collection contains the proxy information (Key: ip), then ignore
     if len([i for i in all_storage.find({"ip": one_proxy_dict_data["ip"]})]) > 0:
-        print("Found duplicate proxy for %s, ignore..." % one_proxy_dict_data["ip"])
-        return
-
-    # Since this ip is new, then store this proxy data in "new" collection, and store only its ip in all storage
-    log_print("Store " + one_proxy_dict_data["ip"])
-    new_storage.insert(one_proxy_dict_data)
-    all_storage.insert({"ip" : one_proxy_dict_data["ip"]})
+        log_print("Found duplicate proxy for %s, ignore..." + one_proxy_dict_data["ip"])
+    else:
+        # Since this ip is new, then store this proxy data in "new" collection, and store only its ip in all storage
+        log_print("Store " + one_proxy_dict_data["ip"])
+        new_storage.insert(one_proxy_dict_data)
+        all_storage.insert({"ip" : one_proxy_dict_data["ip"]})
